1b760d0437ddbf71b71f34e5090625dd20573407
heisenberg550 <150204095@kku.edu.tr>
2023-03-21 02:23:44 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+// 1
+const num1 =  31;
+const num2 =  31; 
+const multiply = num1 + num2;
+// 2
+const random = Math.round((Math.random() * 10) + 1);
+// 3
+const num3 = 200;
+const num4 = 14;
+const mod = num3 % num4
+// 4
+const max = Math.max(1,2,3,4,5,6,7,8,9,20)
